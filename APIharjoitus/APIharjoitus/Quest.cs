fe86e4a022efb880ba5a28003db75acf659843ad
Mukhammad Sakalov <muhis.2005@gmail.com>
2024-10-02 10:05:51 +0300

    Start

---
+++ APIharjoitus/APIharjoitus/Quest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace APIharjoitus
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int reward { get; set; }
+    }
+}
---
+++ APIharjoitus/APIharjoitus/QuestDB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIharjoitus
+{
+    public class QuestDB : DbContext
+    {
+        public QuestDB(DbContextOptions options) : base(options) { }
+
+        public DbSet<Quest> Quests => Set<Quest>();
+    }
+}
